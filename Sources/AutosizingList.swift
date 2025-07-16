98c8d2ab195506b087fd2a051d978a27d6a81f2e
Float-Tech-LLC <jayvanbuiten@gmail.com>
2025-07-16 14:51:17 -0400

    support for ios 26

--- Sources/AutosizingList.swift
+++ Sources/AutosizingList.swift
@@ -28,7 +28,7 @@ public struct AutosizingList<Content: View>: View {
         .introspect(.list, on: .iOS(.v15)) { tableView in
             introspectScrollView(tableView)
         }
-        .introspect(.list, on: .iOS(.v16, .v17, .v18)) { collectionView in
+        .introspect(.list, on: .iOS(.v16, .v17, .v18, .v26)) { collectionView in
             introspectScrollView(collectionView)
         }
         .frame(height: tableContentHeight)
